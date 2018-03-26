37468a43e95556a5207623e809e6119b8c797d27
Robert Noble <robnoble@yahoo.com>
2018-03-25 21:00:21 -0700

    Style cleanup for problem 8

--- src/main/scala/euler/Driver.scala
+++ src/main/scala/euler/Driver.scala
@@ -4,8 +4,6 @@ object Driver {
   import one_to_fifty._
 
   def main(args: Array[String]): Unit = {
-
-
     val answer = LargestProductInSeries.calculate(LargestProductInSeries.series, 13)
 
     println(s"Answer = ${answer}")
